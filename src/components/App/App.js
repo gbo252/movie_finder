745da9b44e824cc7da24125b573e731211a14ae8
gbo252 <guy.owen@virginmedia.com>
2019-08-12 17:00:25 +0100

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Movie Finder</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
