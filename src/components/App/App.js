66f54391b70aa53f82ca7eed491b7c2545954f57
gbo252 <guy.owen@virginmedia.com>
2019-08-13 21:45:42 +0100

    random search by genre complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,15 +7,31 @@ import Unogs from '../../util/Unogs';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { searchResults: {} };
+    this.state = {
+      searchResults: {},
+      movie: ''
+    };
     this.search = this.search.bind(this);
+    this.randomizeMovie = this.randomizeMovie.bind(this);
   }
 
   search(genre) {
-    Unogs.search(genre).then(response => {
-      console.log(genre);
-      this.setState({ searchResults: response });
-    });
+    if (this.state.searchResults[genre]) {
+      console.log(`No search needed for genre: ${genre}`);
+      this.setState({ movie: this.randomizeMovie(genre) });
+    } else {
+      console.log(`Search performed for genre: ${genre}`);
+      Unogs.search(genre).then(response => {
+        let searchObj = this.state.searchResults;
+        searchObj[genre] = response;
+        this.setState({ searchResults: searchObj });
+      }).then(() => this.setState({ movie: this.randomizeMovie(genre) }));
+    }
+  }
+
+  randomizeMovie(genre) {
+    let moviesArr = this.state.searchResults[genre];
+    return moviesArr[Math.floor(Math.random() * moviesArr.length)].title;
   }
 
   render() {
@@ -24,7 +40,7 @@ class App extends React.Component {
         <h1>Netflix Random Movie Generator</h1>
         <img src={require('./logo.png')} alt="logo" />
         <GenreList onSearch={this.search} />
-        <SearchResults movies={this.state.searchResults} />
+        <SearchResults movie={this.state.movie} />
       </div>
     )
   }
