e1fd5543cbf80c922e1e1da55f643142d2e8df94
gbo252 <guy.owen@virginmedia.com>
2019-08-13 21:45:42 +0100

    random search by genre complete

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -3,15 +3,9 @@ import React from 'react';
 class SearchResults extends React.Component {
     render() {
         return (
-            <ul>
-                {this.props.movies.map(movie => {
-                    return <li>{movie.title}</li>
-                    })}
-            </ul>
+            <h3>{this.props.movie.replace(/&#39;/g, "'")}</h3>
         )
     }
 }
 
 export default SearchResults;
-
-//this.props.movies[Math.floor(Math.random() * this.props.movies.length)]
