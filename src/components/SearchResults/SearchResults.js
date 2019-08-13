9ab103501931d220acdf487fff339ea569a44e6a
gbo252 <guy.owen@virginmedia.com>
2019-08-13 23:49:18 +0100

    add loading and more info - working as should

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -3,7 +3,18 @@ import React from 'react';
 class SearchResults extends React.Component {
     render() {
         return (
-            <h3>{this.props.movie.replace(/&#39;/g, "'")}</h3>
+            <div className="row d-flex justify-content-center align-content-center">
+                <div className="col-5">
+                    <h3>{(this.props.movie.title || '').replace(/&#39;/g, "'")}</h3>
+                    <h5>Synopsis:</h5><p>{(this.props.movie.synopsis || '').replace(/&#39;/g, "'")}</p>
+                    <h5>Runtime:</h5><p>{this.props.movie.runtime}</p>
+                    <h5>Released:</h5><p>{this.props.movie.released}</p>
+                    <h5>Rating:</h5><p>{this.props.movie.rating}</p>
+                </div>
+                <div className="col-3">
+                    <img src={this.props.movie.image} alt={this.props.movie.title}></img>
+                </div>
+            </div>
         )
     }
 }
