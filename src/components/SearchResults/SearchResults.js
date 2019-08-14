309529142d1ebf3c97b85521922ddfcc430db894
gbo252 <guy.owen@virginmedia.com>
2019-08-14 17:06:46 +0100

    renderOptions and inc multiple genre codes

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -6,7 +6,7 @@ class SearchResults extends React.Component {
             <div className="row d-flex justify-content-center align-content-center">
                 <div className="col-5">
                     <h3>{(this.props.movie.title || '').replace(/&#39;/g, "'")}</h3>
-                    <h5>Synopsis:</h5><p>{(this.props.movie.synopsis || '').replace(/&#39;/g, "'")}</p>
+                    <h5>Synopsis:</h5><p>{(this.props.movie.synopsis || '').replace(/&#39;/g, "'").replace(/&rsquo;/g, "’")}</p>
                     <h5>Runtime:</h5><p>{this.props.movie.runtime}</p>
                     <h5>Released:</h5><p>{this.props.movie.released}</p>
                     <h5>Rating:</h5><p>{this.props.movie.rating}</p>
