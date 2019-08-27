e83c105d7468afa5f3b99003902bcfc69292fd01
gbo252 <guy.owen@virginmedia.com>
2019-08-27 15:04:12 +0100

    add tooltips

--- src/components/GenreList/GenreList.js
+++ src/components/GenreList/GenreList.js
@@ -30,14 +30,16 @@ class GenreList extends React.Component {
 
     renderButton() {
         let atts = {};
-        if (this.state.genre === "X") { atts.disabled = true }
+        if (this.state.genre === "X") { atts.disabled = true; atts.title = "Select a genre" }
         if (this.props.loading) {
             return <button className="btn btn-danger" type="button" disabled>
                 <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
                 Loading...
             </button>
         } else {
-            return <button onClick={this.handleSearch} className="btn btn-danger" {...atts}>Find Movie</button>
+            return <span {...atts}>
+                <button onClick={this.handleSearch} className="btn btn-danger" {...atts}>Find Movie</button>
+            </span>
         }
     }
 
